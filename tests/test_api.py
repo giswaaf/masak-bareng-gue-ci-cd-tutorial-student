4174368155d1e32ed57ddc6683ae09e9a0c664c9
giswaaf <fawwazgiswa@gmail.com>
2026-06-04 13:40:34 +0700

    test: sengaja break test untuk demo

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,7 +17,7 @@ client = TestClient(app)
 class TestHomepage:
     def test_returns_200(self):
         res = client.get("/")
-        assert res.status_code == 200
+        assert res.status_code == 9999
 
     def test_contains_recipe_content(self):
         res = client.get("/")
